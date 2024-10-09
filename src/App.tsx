3b5348275cddbeab6477d3e361340175f39a5baa
Tarek Ramadan <tramadan.up@gmail.com>
2024-10-09 15:43:23 +0200

    reconfig routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage.tsx";
 import CoordinatorMeetingCreationPage from "./pages/CoordinatorMeetingCreationPage.tsx";
 import ParticipantWaitingPage from "./pages/ParticipantWaitingPage.tsx";
